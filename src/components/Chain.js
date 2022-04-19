28902e75de1703babb10a1db89e5e66e299a3d58
rahul925kumar <rahulpassi925@gmail.com>
2022-04-19 15:54:34 +0530

    chain bounce

--- src/components/Chain.js
+++ src/components/Chain.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import Matter from "matter-js";
 import chainpieces from '../resized/9kt_gold_short_chain_1.png';
-import locket from '../resized/LC2130-Y (1) (2).png';
+import locket from '../resized/LC2130-Y (1) (1).png';
 import ImgSvg from '../resized/New-Project-_12_.svg';
-import joint from '../resized/link (1).png';
+import joint from '../resized/LC2130-Y (2) (4).png';
 
 const Chain = () => {
     useEffect(() => {
@@ -16,6 +16,7 @@ const Chain = () => {
             Constraint = Matter.Constraint,
             Common = Matter.Common,
             Svg = Matter.Svg,
+            Query = Matter.Query,
             MouseConstraint = Matter.MouseConstraint,
             Mouse = Matter.Mouse,
             Bodies = Matter.Bodies;
@@ -27,9 +28,9 @@ const Chain = () => {
             engine: engine,
             options: {
                 width: 1500,
-                height: 600,
+                height: 800,
                 wireframes: false,
-                // background: '#fff'
+                background: '#fff'
             }
         });
         Render.run(render);
@@ -37,13 +38,13 @@ const Chain = () => {
         Runner.run(runner, engine);
         var group = Body.nextGroup(true);
         var count = 0;
-        var ropeA = Composites.stack(100, 10, 23, 1, -5, 15, function (x, y) {
+        var ropeA = Composites.stack(100, 10, 19, 1, -5, 15, function (x, y) {
             count++;
-            if (count == '12') {
-                return Bodies.circle(415, 15, 60, {
+            if (count == '9') {
+                return Bodies.circle(x, 15, 10, {
                     frictionAir: 0.005, render: {
                         sprite: {
-                            texture: locket
+                            texture: joint
                         }
                     }
                 });
@@ -66,40 +67,35 @@ const Chain = () => {
             length: 1,
         }));
         Composite.add(ropeA, Constraint.create({
-            bodyB: ropeA.bodies[22],
+            bodyB: ropeA.bodies[18],
             pointB: { x: 0, y: -20 },
             pointA: { x: 400, y: 20 },
             stiffness: 0.5,
             length: 1,
         }));
-        /* var bodyA = Bodies.polygon(500, 400, 6, 30); */
-        var bodyB = Bodies.polygon(600, 400, 7, 60); 
-        var pendant = Bodies.rectangle(415, 15, 70, 90, {
-            frictionAir: 0.005, render: {
+        var bodyB = Bodies.circle(400, 400, 60, {
+            collisionFilter: { group: -1 },
+            static: true,
+            render: {
                 sprite: {
                     texture: locket
                 }
             }
         });
-
-        var constraint = Constraint.create({
-            bodyA: ropeA.bodies[11],
+        var constraint1 = Constraint.create({
+            bodyA: ropeA.bodies[8],
             pointA: { x: 0, y: 0 },
-            bodyB: pendant,
-            pointB: { x: 0, y: 0 },
-            stiffness: 0.002,
-            damping: 0.1,
-            length: 1.
+            bodyB: bodyB,
+            pointB: { x: -10, y: 10 },
+            length: 40,
+            stiffness: 0.03
         });
 
-        /* Composite.add(ropeA, Constraint.create({
-            bodyB: pendant,
-            pointB: { x: 0, y: 0 },
-            pointA: { x: 275, y: 475 },
-            length: 1,
-        })); */
+
+        Composite.add(world, [bodyB, constraint1]);
+
         group = Body.nextGroup(true);
-        Composite.add(world, [ropeA,  Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true })]);
+        Composite.add(world, [ropeA, Bodies.rectangle(400, 600, 1700, 50.5, { isStatic: true })]);
         var mouse = Mouse.create(render.canvas),
             mouseConstraint = MouseConstraint.create(engine, {
                 mouse: mouse,
@@ -111,7 +107,7 @@ const Chain = () => {
                 }
             });
 
-       /*  if (typeof fetch !== 'undefined') {
+        if (typeof fetch !== 'undefined') {
             var select = function (root, selector) {
                 return Array.prototype.slice.call(root.querySelectorAll(selector));
             };
@@ -122,22 +118,32 @@ const Chain = () => {
             };
             loadSvg(ImgSvg)
                 .then(function (root) {
-                    console.log("TEST===", root);
                     var paths = select(root, 'path');
                     var vertexSets = paths.map(function (path) { return Svg.pathToVertices(path, 10); });
                     var terrain = Bodies.fromVertices(200, 310, vertexSets, {
-                        isStatic: true,
+                        //  isStatic: true,
                         render: {
                             fillStyle: 'transparent',
-                            strokeStyle: '#060a19',
+                            strokeStyle: '#fff',
                             lineWidth: 1
                         }
                     }, true);
-                    Composite.add(engine.world, terrain);
+                    // return terrain;
+                    // Composite.add(engine.world, terrain);
+                    var bodyOptions = {
+                        frictionAir: 0,
+                        friction: 0.0001,
+                        restitution: 0.6
+                    };
+                    Composite.add(world, Composites.stack(80, 100, 10, 20, 10, 10, function (x, y) {
+                        if (Query.point([terrain], { x: x, y: y }).length === 0) {
+                            return Bodies.rectangle(x, y, 15, 15, bodyOptions);
+                        }
+                    }));
                 });
         } else {
             Common.warn('Fetch is not available. Could not load SVG.');
-        } */
+        }
 
         Composite.add(world, mouseConstraint);
         render.mouse = mouse;
