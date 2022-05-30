8bd267bb3f0ab1e12d01f4eec10b818186df6a3b
rahul925kumar <rahulpassi925@gmail.com>
2022-05-30 14:43:24 +0530

    checkup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-/* import Matter from './components/Locket'
+import Matter from './components/Locket'
 import Check from './components/Test'
 import TerrianExample from './components/Terrian';
 import CircleLocket from './components/CircleLocket';
 import HeartLocket from './components/HeartLocket';
 import OvalLocket from './components/OvalLocket';
-import Instafeed from './components/Instafeed';
 import Demo from './components/Demo';
 import Login from './components/Login'
 import Chain from './components/Chain';
-import SignUp from './components/SignUp'; */
+import SignUp from './components/SignUp';
 import Insta from './components/Instafeed';
 import { AppProvider } from '@shopify/polaris';
 import en from '@shopify/polaris/locales/en.json';
@@ -20,17 +19,16 @@ function App() {
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="test" exact element={<Check />} />
+          <Route path="test" exact element={<Check />} />
           <Route path="chain" element={<Chain />} />
           <Route path="demo" element={<Demo />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="circle" element={<CircleLocket />} />
-          <Route path='heart' element={<HeartLocket />} /> */}
+          <Route path='heart' element={<HeartLocket />} />
           <Route path="insta" exact element={<Insta />} />
         </Routes>
       </BrowserRouter> 
     </ AppProvider>
   );
 }
-
 export default App;
